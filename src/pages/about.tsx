5df406474799a9b466115bf86a3f6ae97d734c68
Vivek-Prajapatii <prajapativivek415@gmail.com>
2023-09-22 02:02:04 +0530

    updated the about us page and updated the profile and made the profile section dynamic using json

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,18 @@
-import { Grid, Container, Typography } from "@mui/material";
+/* eslint-disable @next/next/no-img-element */
+import {
+  Grid,
+  Container,
+  Typography,
+  Button,
+  IconButton,
+  Stack,
+} from "@mui/material";
 import scss from "../styles/pages/about.module.scss";
 import React from "react";
-
+import Management from "../../public/Items.json";
+import EmailIcon from "@mui/icons-material/Email";
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import InstaIcon from "/images/Insta.svg";
 function about() {
   return (
     <div>
@@ -9,7 +20,7 @@ function about() {
         <Grid className={scss.outer_grid}>
           <Grid className={scss.title}>
             <Grid className={scss.inputwrapper}>
-              <span className={scss.placeholder}></span>
+              <span className={scss.who_are_we}></span>
             </Grid>
           </Grid>
           <Grid className={scss.about_us}>
@@ -26,16 +37,58 @@ function about() {
         <Grid className={scss.people}>
           <Grid className={scss.title}>
             <Grid className={scss.inputwrapper}>
-              <span className={scss.placeholder}></span>
+              <span className={scss.management_team}></span>
             </Grid>
           </Grid>
-          <Grid
-            className={scss.cards}
-          >
-            <Grid className={scss.card}></Grid>
-            <Grid className={scss.card}></Grid>
-            <Grid className={scss.card}></Grid>
-            <Grid className={scss.card}></Grid>
+          <Grid className={scss.cards}>
+            {Management.managementTeam.map((member: any) => {
+              return (
+                <Grid className={scss.card} key={member.id}>
+                  <Grid className={scss.data}>
+                    <Typography
+                      variant={"h5"}
+                      sx={{
+                        color: "white",
+                        fontFamily: "monospace",
+                        m: "2rem",
+                      }}
+                    >
+                      {member.name}
+                    </Typography>
+                    <Typography variant={"body1"} className={scss.member_info}>
+                      {"- " + member.role}
+                    </Typography>
+                    <Typography variant={"body1"} className={scss.member_info}>
+                      {member.contact}
+                    </Typography>
+                    <Typography variant={"body1"} className={scss.member_info}>
+                      {member.place}
+                    </Typography>
+                    <Stack
+                      sx={{
+                        width: "inherit",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <IconButton color={"primary"}>
+                        <EmailIcon />
+                      </IconButton>
+                      <IconButton color={"primary"}>
+                        <FacebookTwoToneIcon />
+                      </IconButton>
+                      <IconButton color={"primary"}>
+                        <img src="/images/insta.svg" alt="insta icon" />
+                      </IconButton>
+                    </Stack>
+                  </Grid>
+                  <Grid item className={scss.image}>
+                    <img src={member.imageUrl} alt="insta icon" />
+                  </Grid>
+                </Grid>
+              );
+            })}
           </Grid>
         </Grid>
       </Container>
