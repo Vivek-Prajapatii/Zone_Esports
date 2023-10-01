e202739ed380fae37521e5af1893107945d9b6a8
Vivek-Prajapatii <prajapativivek415@gmail.com>
2023-10-01 22:37:29 +0530

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,35 @@
-import { Container, Grid, Stack, IconButton } from "@mui/material";
+import { Container, Grid, Stack, IconButton, Typography } from "@mui/material";
 import React from "react";
 import scss from "../styles/components/Footer.module.scss";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import MailIcon from "@mui/icons-material/Mail";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 
 function Footer() {
   return (
     <Container className={scss.container} maxWidth="xl">
       <Grid className={scss.grid_container}>
         <Grid className={scss.copyright}>
-          © 2023 Zone Esports, India, All rights reserved
+          © 2023 <b>Zone Esports</b>, India, All rights reserved
         </Grid>
         <Grid className={scss.socials}>
-          <Stack direction="row">
-            <IconButton aria-label="delete">
+          <Stack direction="row" sx={{display:"flex", alignItems:"center"}} spacing={"1rem"}>
+            <Typography variant="body1">Follow us on socials</Typography>
+            <IconButton>
+              <YouTubeIcon />
+            </IconButton>
+            <IconButton>
               <InstagramIcon />
             </IconButton>
-            <IconButton aria-label="delete">
+            <IconButton>
               <FacebookIcon />
             </IconButton>
-            <IconButton aria-label="delete">
+            <IconButton>
               <TwitterIcon />
             </IconButton>
-            <IconButton aria-label="delete">
+            <IconButton>
               <MailIcon />
             </IconButton>
           </Stack>
